e05af05a4c7e88d73d73978655a36040ce383bde
Carl Tessier <doktorfrankenstein@gmail.com>
2020-07-03 01:44:28 -0400

    Add MainWindow

--- avpipe.cpp
+++ avpipe.cpp
@@ -2,17 +2,20 @@
 #include <QPushButton>
 extern "C"
 {
-#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 }
 
 #include "avpipe.h"
+#include "mainwindow.hpp"
 
 int main(int argc, char* argv[])
 {
 	QApplication app { argc, argv };
 
-	QPushButton button { "Hello, World!" };
-	button.show();
+	// av_register_all();	// deprecated
+
+	MainWindow mainwin;
+	mainwin.show();
 
 	return app.exec();
 }
