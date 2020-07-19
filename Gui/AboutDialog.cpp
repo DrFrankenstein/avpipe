8ab71b787bc291aa2bd66d5f6c7a3de9f04c5354
Carl Tessier <doktorfrankenstein@gmail.com>
2020-07-19 02:16:28 -0400

    Implement AboutDialog

--- Gui/AboutDialog.cpp
+++ Gui/AboutDialog.cpp
@@ -1,11 +1,16 @@
 #include "AboutDialog.hpp"
 #include "ui_AboutDialog.h"
+#include "../config.h"
 
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+	QString versionText = ui->labelVersion->text().arg(PROJECT_VERSION);
+	ui->labelVersion->setText(versionText);
+	QString buildText = ui->labelBuild->text().arg(PROJECT_BUILD);
+	ui->labelBuild->setText(buildText);
 }
 
 AboutDialog::~AboutDialog()
