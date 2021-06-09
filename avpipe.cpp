cfed3d985c66bde6b1b3ce7148461b2bfad63b4a
Carl Tessier <doktorfrankenstein@gmail.com>
2021-06-09 10:34:07 -0400

    feat: MORE LOGGING
    Developers like logging.

--- avpipe.cpp
+++ avpipe.cpp
@@ -3,6 +3,7 @@
 extern "C"
 {
 #include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
 }
 
 #include "avpipe.h"
@@ -13,6 +14,7 @@ int main(int argc, char* argv[])
 	QApplication app { argc, argv };
 
 	// av_register_all();	// deprecated
+	::av_log_set_level(AV_LOG_DEBUG);
 
 	MainWindow mainwin;
 	mainwin.show();
